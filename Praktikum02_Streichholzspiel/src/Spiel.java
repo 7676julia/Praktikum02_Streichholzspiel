d6d03ff355116c98c65fccb6a70c8f40f7699fce
ju <164335052+7676julia@users.noreply.github.com>
2024-04-19 10:43:58 +0200

    create Spiel

---
+++ Praktikum02_Streichholzspiel/src/Spiel.java
@@ -0,0 +1,28 @@
+public class Spiel {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    //Datenfelder
+    private int currentAnzahlHoelzer = 0;
+
+    //Konstruktor
+    public Spiel(int startAnzahlHoelzer) {
+        while (currentAnzahlHoelzer != 0) {
+            computerZiehen(1);
+            menschZiehen(1);
+        }
+    }
+
+    //Methoden
+    public void computerZiehen (int anzahlHoelzer) {
+        // Method code here
+    }
+    public void menschZiehen (int anzahlHoelzer) {
+        // Method code here
+    }
+    private void berechneComputerZug() {
+        // Method code here
+    }
+}
+
