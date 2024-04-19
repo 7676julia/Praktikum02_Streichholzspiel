e4b7a3ef8ae522ae1098c3e05c0f57e32ab20311
ju <164335052+7676julia@users.noreply.github.com>
2024-04-19 11:36:12 +0200

    add Spiel

---
+++ Praktikum02_Streichholzspiel/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Spiel spiel = new Spiel(18);
+        
+        //testen
+        System.out.println("Hello, World!");
+        Eingabe.leseHoelzer();
+    }
+}
--- Praktikum02_Streichholzspiel/src/Spiel.java
+++ Praktikum02_Streichholzspiel/src/Spiel.java
@@ -1,10 +1,5 @@
 public class Spiel {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        Eingabe.leseHoelzer();
-    }
-
+    
     //Datenfelder
     private int currentAnzahlHoelzer = 0;
 
